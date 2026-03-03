2f1dcea16f55ceb8b3dae6650ec33991844eef21
Matthew Long <mtl2165@columbia.edu>
2026-03-03 14:15:49 -0500

    Update file with simplified logic

--- error_rx.sv
+++ error_rx.sv
@@ -30,39 +30,32 @@ module error_rx #(parameter[15:0] crc_polynomial,
     output logic packet_done, //Output to show end of packet
     output logic error        //High output indicates an error in the packet
     );
-    //FSM
-    typedef enum {CALC_CRC, READ_CRC} state;
-    state current_state, next_state;
     
     //Bit tracking
     logic [6:0] bit_counter;
-    
+
     //Crc data
-    logic [15:0] crc_next;
-    logic [15:0] crc_val;
-    logic [15:0] rx_crc;
+    logic [15:0] crc_comb;
+    logic [15:0] crc_reg;
+    
      
-    //shift serial data into shift register, sequential data processing
+     
+    //Sequential data
     always_ff @(posedge clk_data) begin
         if (reset) begin
             bit_counter <= 7'd0;
-            crc_val <= init;
-            rx_crc <= 16'd0;
+            crc_reg <= init;
         end else begin
-            //upon startup, initiate the values, needed to delay the start of the operation by 1 clock cycle
+
             if (data_valid) begin
-                crc_val <= init; 
-                bit_counter <= 7'd0;
-            end else begin
-                crc_val <= crc_next;
+                crc_reg <= crc_comb;
                 bit_counter <= bit_counter + 1; 
                 
                 if (bit_counter == 7'd63) begin
                     bit_counter <= 7'd0;
-                    crc_val <= init;
+                    crc_reg <= init;
                 end
                 
-                rx_crc <= {rx_crc[14:0], data_in};
             end  
         end
     end
@@ -70,52 +63,25 @@ module error_rx #(parameter[15:0] crc_polynomial,
     //Contains combinational logic
     always_comb begin
         //default values
-        next_state = current_state;
         packet_done = 0;
         error = 0;
-        crc_next = crc_val;
+        crc_comb = crc_reg;
         
         if (!reset) begin
-            case (current_state)
-                CALC_CRC: begin
                 //CRC Calculation algorithm
-                    if (crc_val[15] ^ data_in) begin
-                        crc_next = {crc_val[14:0], 1'b0} ^ crc_polynomial;
-                    end else begin
-                        crc_next = {crc_val[14:0], 1'b0};
-                    end
-                    //Next state
-                    if (bit_counter == 7'd47) begin
-                        next_state = READ_CRC;
-                    end
-                    
+                if (crc_reg[15] ^ data_in) begin
+                    crc_comb = {crc_reg[14:0], 1'b0} ^ crc_polynomial;
+                end else begin
+                    crc_comb = {crc_reg[14:0], 1'b0};
                 end
-                
-                READ_CRC: begin
-                    //Complete comparison once packet is finished
-                    if (bit_counter == 7'd63) begin
-                        error = crc_val != rx_crc;
-                        packet_done = 1;
-                        next_state = CALC_CRC;
-                        crc_next = 16'd0;
-                    end else begin
-                        error = 0;
-                        packet_done = 0;
-                    end
                     
+                if (bit_counter == 7'd63) begin
+                    packet_done = 1;
+                    error = (crc_comb != 16'h0000);
                 end
-            endcase
         end
     end
     
-    //Sequential logic (switching between states)
-    always_ff @(posedge clk_data) begin
-        if (reset) begin
-            current_state <= CALC_CRC;
-        end else begin
-            current_state <= next_state;
-        end 
-    end
     
    
 endmodule
